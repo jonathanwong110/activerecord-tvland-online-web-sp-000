a1a1b81e3bbdada7118dc911ae013d78d5e5cab9
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-19 14:20:06 +0000

    Automatically backed up by Learn

--- db/migrate/20190618042910_create_characters.rb
+++ db/migrate/20190618042910_create_characters.rb
@@ -3,10 +3,6 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
-      t.string :catchphrase
-      t.string :show
-      t.string :build_show
-      t.integer :actor_id
     end
   end
 end
