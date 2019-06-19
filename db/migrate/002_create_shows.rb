93f352a64e5d2825b6f933e810af2c06c8c69873
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-19 14:22:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_shows.rb
+++ db/migrate/002_create_shows.rb
@@ -4,7 +4,6 @@ class CreateShows < ActiveRecord::Migration[4.2]
       #primary key of :id is created for us!
       t.string :name
       t.string :genre
-      t.string :call_letters
       t.string :day
       t.string :season
     end
