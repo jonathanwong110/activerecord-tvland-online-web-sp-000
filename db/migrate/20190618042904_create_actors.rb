1f19c0bf854200ab7bb89aff02c494e643b1724c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-19 14:24:22 +0000

    Automatically backed up by Learn

--- db/migrate/20190618042904_create_actors.rb
+++ db/migrate/20190618042904_create_actors.rb
@@ -3,6 +3,8 @@ class CreateActors < ActiveRecord::Migration[4.2]
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
+      t.string :shows
+      t.string :characters
     end
   end
 end
